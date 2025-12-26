6fc5a98933a15b40da7fcab06cc6e99571403e64
Timonin <timonin.9.8x@gmail.ru>
2025-12-26 20:04:26 +0400

    Сделано меню и добавлен класс Car

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,34 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        boolean check = true;
+        while (check) {
+            System.out.println("Выберите пункт меню: \n" +
+                    "1 - Пункт 1 \n" +
+                    "2 - Пункт 2 \n" +
+                    "0 - Выход \n" +
+                    "Ваш выбор:");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            Scanner in = new Scanner(System.in);
+            String menu = in.nextLine();
+
+            switch(menu){
+                case "1":{
+                    System.out.println("Пункт 1");
+                    break;
+                }
+                case "2":{
+                    System.out.println("Пункт 2");
+                    break;
+                }
+                case "0":{
+                    check = false;
+                    break;
+                }
+                default:
+                    System.out.println("Введите корректное значение!");
+            }
         }
     }
 }
